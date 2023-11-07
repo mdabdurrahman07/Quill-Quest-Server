a76d56df8c563b6f30bd4eee375c7d14fb299286
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-11-07 12:22:44 +0600

    server created and post and get method added

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ async function run() {
         const result = await QuillQuestCollections.insertOne(allData)
         res.send(result)
     })
+    app.get('/allServices' , async(req , res)=>{
+        const result = await QuillQuestCollections.find().toArray()
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
