99ab3f45de33599f4e73baeafd01fca094253c01
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-11-08 23:19:26 +0600

    vercel deploy reated issue fixed

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const QuillQuestCollections = client.db('QuillQuestDB').collection('Services')
     const QQBookingCollections = client.db('QuillQuestDB').collection('Booking')
     app.post('/allServices' , async(req , res)=>{
